754aa48c248ef9cc44da9eee3582de30b58341f4
Anna <95273086+annettalekto@users.noreply.github.com>
2022-08-10 16:28:36 +0300

    about program

--- ztc.go
+++ ztc.go
@@ -1,22 +1,100 @@
 package main
 
 import (
+	"fmt"
+	"os/exec"
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/storage"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
+var gVersion, gYear, gProgramName string
+
 func main() {
+	gProgramName = "Электронная имитация параметров"
+
 	a := app.New()
-	w := a.NewWindow("Zettelkasten")
+	w := a.NewWindow(gProgramName)
 	w.Resize(fyne.NewSize(700, 500))
+	w.CenterOnScreen()
+	w.SetMaster()
+
+	menu := fyne.NewMainMenu(
+		fyne.NewMenu("Файл",
+			// a quit item will be appended to our first menu
+			fyne.NewMenuItem("Тема", func() { changeTheme(a) }),
+			// fyne.NewMenuItem("Выход", func() { a.Quit() }),
+		),
+
+		fyne.NewMenu("Справка",
+			fyne.NewMenuItem("Посмотреть справку", func() { aboutHelp() }),
+			// fyne.NewMenuItemSeparator(),
+			fyne.NewMenuItem("О программе", func() { abautProgramm() }),
+		),
+	)
+	w.SetMainMenu(menu)
+
+	go func() { // простите
+		time.Sleep(1 * time.Second)
+		for _, item := range menu.Items[0].Items {
+			if item.Label == "Quit" {
+				item.Label = "Выход"
+			}
+		}
+	}()
 
 	// прога для работы с сиситемой Zettelkasten
 	// будет сохранять файлы в определенном виде, ну и читать их
-	// тест git
-	// конфликт
-	// merge конфликт = поправлено
 
 	w.SetContent(widget.NewLabel("Let's start!"))
 	w.ShowAndRun()
+}
+
+var currentTheme bool // светлая тема false
+
+func changeTheme(a fyne.App) {
+	currentTheme = !currentTheme
+
+	if currentTheme {
+		a.Settings().SetTheme(theme.DarkTheme())
+	} else {
+		a.Settings().SetTheme(theme.LightTheme())
+	}
+}
+
+func aboutHelp() {
+	err := exec.Command("cmd", "/C", ".\\help\\index.html").Run()
+	if err != nil {
+		fmt.Println("Ошибка открытия файла справки")
+	}
+}
+
+func abautProgramm() {
+	w := fyne.CurrentApp().NewWindow("О программе") // CurrentApp!
+	w.Resize(fyne.NewSize(400, 150))
+	w.SetFixedSize(true)
+	w.CenterOnScreen()
+
+	img := canvas.NewImageFromURI(storage.NewFileURI("ind.png"))
+	img.Resize(fyne.NewSize(66, 90)) //без изменений
+	img.Move(fyne.NewPos(10, 10))
+
+	l0 := widget.NewLabel(gProgramName)
+	l0.Move(fyne.NewPos(80, 10))
+	l1 := widget.NewLabel(fmt.Sprintf("Версия %s", gVersion))
+	l1.Move(fyne.NewPos(80, 40))
+	l2 := widget.NewLabel(fmt.Sprintf("© Anna, %s", gYear))
+	l2.Move(fyne.NewPos(80, 70))
+
+	box := container.NewWithoutLayout(img, l0, l1, l2)
+
+	// w.SetContent(fyne.NewContainerWithLayout(layout.NewCenterLayout(), box))
+	w.SetContent(box)
+	w.Show() // ShowAndRun -- panic!
 }
