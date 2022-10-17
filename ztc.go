010d4d563fc73b517cf16ad032f798136c75e1c3
Anna <95273086+annettalekto@users.noreply.github.com>
2022-10-17 11:17:53 +0300

    test mod 2

--- ztc.go
+++ ztc.go
@@ -7,12 +7,10 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/theme"
-
 	"fyne.io/fyne/v2/widget"
 )
 
